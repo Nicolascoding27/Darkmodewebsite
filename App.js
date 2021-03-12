83bad1c7a15aec4e144d8a0d236fef233693e404
NICOLAS DIAZ <nicolas.diaz27@hotmail.com>
2021-03-11 22:59:03 -0500

    bug fixed, webpage done

--- App.js
+++ App.js
@@ -1,4 +1,9 @@
 const button=document.querySelector('.theme-button')
 button.addEventListener('click',() =>{
     document.body.classList.toggle('dark');
-})
+
+const sunMoonContainer=document.querySelector('.sun-moon-container')
+     const currentRotation=parseInt(getComputedStyle(sunMoonContainer)
+     .getPropertyValue('--rotation'))
+     sunMoonContainer.style.setProperty('--rotation', currentRotation+180 )
+    })
